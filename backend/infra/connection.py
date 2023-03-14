369f957372c7eb8849a2d5f33b2ae8973f5d8635
Marc-Anthony Girard <marcanthony.girard.pro@gamil.com>
2023-03-13 22:48:05 -0400

    does<t work but still better

--- backend/infra/connection.py
+++ backend/infra/connection.py
@@ -1,10 +1,9 @@
 import mysql.connector
 
-
 class Connection:
-
     def __init__(self):
         self.connection = self.getConnection()
+
     def getConnection(self):
         return mysql.connector.connect(host="localhost", user="root", password="")
 
--- backend/infra/userRepository.py
+++ backend/infra/userRepository.py
@@ -1,18 +1,20 @@
 from domain.user import UserRequest
 from infra.connection import Connection
 class UserRepository:
+
+    def __init__(self):
+        self.connection = Connection()
     def create_user(self, user: UserRequest):
-        Connection = Connection()
-        Connection.exexecute("INSERT INTO user (name, role, description, image) VALUES ('" + user.name + "', '" + user.role + "', '" + user.description + "', '" + user.image + "')")
+        self.connection.exexecute("INSERT INTO user (name, role, description, image) VALUES ('" + user.name + "', '" + user.role + "', '" + user.description + "', '" + user.image + "')")
 
     def get_user(self, id):
-        return Connection.exexecute("SELECT * FROM user WHERE id = " + id)
+        return self.connection.exexecute("SELECT * FROM user WHERE id = " + id)
 
     def get_all_users(self):
-        return Connection.exexecute("SELECT * FROM user")
+        return self.connection.exexecute("SELECT * FROM user")
 
     def update_user(self, user, user_id):
-        Connection.exexecute("UPDATE user SET name = '" + user.name + "', role = '" + user.role + "', description = '" + user.description + "', image = '" + user.image + "' WHERE id = " + user_id)
+        self.connection.exexecute("UPDATE user SET name = '" + user.name + "', role = '" + user.role + "', description = '" + user.description + "', image = '" + user.image + "' WHERE id = " + user_id)
         
     def delete_user(self, id):
-        Connection.exexecute("DELETE FROM user WHERE id = " + id)
+        self.connection.exexecute("DELETE FROM user WHERE id = " + id)
