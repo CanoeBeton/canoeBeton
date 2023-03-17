1c6bfda5ecf4352b38c81b337945db1a29465a8e
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-03-17 13:38:44 -0400

    making db-init work

--- backend/init/08_years_partenaires.sql
+++ backend/init/08_years_partenaires.sql
@@ -2,7 +2,7 @@ drop table if exists partenaires;
 
 CREATE TABLE partenaires (
     year varchar(255) NOT NULL,
-    partenaire_name TEXT NOT NULL,
+    partenaire_name varchar(255) NOT NULL,
     type ENUM('bronze', 'argent', 'gold'),
   PRIMARY KEY (year, partenaire_name)
 );
