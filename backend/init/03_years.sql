4f5570f1347e4d31a9803f843a0b742a8ce9e842
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-03-19 19:45:55 -0400

    je sais

--- backend/init/03_years.sql
+++ backend/init/03_years.sql
@@ -3,6 +3,9 @@ drop table if exists years;
 CREATE TABLE years (
   year int(11) NOT NULL,
   boat_name varchar(100) not null,
+  partenaire_mosaique blob,
+  active tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (year)
 );
 
+## vérifier qui'il n'y a pas deux active ON UPDATE
