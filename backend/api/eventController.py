a5f88ef69dc7092eeb5a0d85c2037ef42e737060
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-03-21 21:57:55 -0400

    backend done for event

--- backend/api/eventController.py
+++ backend/api/eventController.py
@@ -2,39 +2,39 @@ from flask import request, jsonify
 
 from api.resource import ApiResource
 from domain.event import EventRequest
-from infra.memberRepository import MemberRepository
+from infra.eventRepository import EventRepository
 
-member_repository = MemberRepository()
+event_repository = EventRepository()
 
 class EventController(ApiResource):
     @staticmethod
     def path():
-        return "/member"
+        return "/event"
 
     def post(self):
         event_json = request.get_json()
         event_request = EventRequest(**event_json)
-        member_repository.create(event_request)
+        event_repository.create(event_request)
         return jsonify({"success": True})
 
     def get(self):
-        members = member_repository.get_all()
-        return jsonify(members.__dict__())
+        events = event_repository.get_all()
+        return jsonify(events.__dict__())
 
 class EventByIdController(ApiResource):
     @staticmethod
     def path():
-        return "/member/<id>"
+        return "/event/<id>"
 
     def get(self, id):
-        member = member_repository.get(id)
+        member = event_repository.get(id)
         return jsonify(member.__dict__())
 
     def put(self, id):
         member = request.get_json()
-        member_repository.update(member["name"], member["role"], member["description"], member["image"], id)
+        event_repository.update(member["name"], member["role"], member["description"], member["image"], id)
         return jsonify({"success": True})
 
     def delete(self, id):
-        member_repository.delete(id)
+        event_repository.delete(id)
         return jsonify({"success": True})
