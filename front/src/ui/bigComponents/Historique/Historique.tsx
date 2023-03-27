92f099c861f5eef3eb6fb71c6f91cf693a86becc
Marc-Anthony Girard <marcanthony.girard.pro@gamil.com>
2023-03-26 20:04:32 -0400

    /historique done

--- front/src/ui/bigComponents/Historique/Historique.tsx
+++ front/src/ui/bigComponents/Historique/Historique.tsx
@@ -10,13 +10,16 @@ import YearCard from "./YearCard/YearCard";
 interface HeaderProps {}
 
 const Historique: FunctionComponent<PropsWithChildren<HeaderProps>> = ({}) => {
-  const {data: year, error} = useQuery({ queryKey: ['year'], queryFn: () => getYears() })
+  const {data: year, status} = useQuery({ queryKey: ['year'], queryFn: () => getYears() })
 
   return (
     <div className={styles.page}>
       <span className={styles.page_title}>Historique</span>
       <div className={styles.tournament_container}>
-        {year?.map((year) => <YearCard year={year}/>)}
+        {status == 'error' && <span>Une erreur est survenue, veillez réessayer plustard</span>}
+        {status == 'loading' && <span> Chargement en cours ! </span>}
+        {year?.sort((a, b) => b.year-a.year).
+        map((year) => <YearCard year={year}/>)}
       </div>
     </div>
   )
