9e5bc020029e47b5a9c2233ce0a7d018c5fb13a5
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-04-11 11:56:12 -0400

    added admin login

---
+++ backend/api/adminController.py
@@ -0,0 +1,15 @@
+from flask import request, jsonify
+
+from api.resource import ApiResource
+from infra.adminRepository import AdminRepository
+
+admin_repository = AdminRepository()
+class AdminController(ApiResource):
+    @staticmethod
+    def path():
+        return "/login"
+
+    def post(self):
+        admin_json = request.get_json()
+        admin_repository.login(admin_json["email"], admin_json["password"])
+        return jsonify({"token": True})
