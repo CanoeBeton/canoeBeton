58370e6316100002f0c06bc6cf3cf50d63a70984
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-04-13 17:35:18 -0400

    repair repository partenaire

--- backend/infra/partenaireRepository.py
+++ backend/infra/partenaireRepository.py
@@ -11,24 +11,24 @@ class PartenaireRepository:
     def create(self, partenaire):
         self.connection.change(f'INSERT INTO {PartenaireRepository.TABLE} (name, description, type, image) VALUES ("{partenaire.name}", "{partenaire.description}" , "{partenaire.type}",  "{partenaire.image}")')
 
-    def get(self, id) -> PartenaireResponse:
-        tuple = self.connection.get(f'SELECT * FROM {PartenaireRepository.TABLE} WHERE name = {id}')
+    def get(self, name) -> PartenaireResponse:
+        tuple = self.connection.get(f'SELECT * FROM {PartenaireRepository.TABLE} WHERE name = "{name}"')
         return PartenaireResponse(tuple[0])
 
     def get_all(self) -> PartenaireResponseList:
         return PartenaireResponseList(self.connection.get(f'SELECT * FROM {PartenaireRepository.TABLE}'))
 
-    def update(self, partenaire, partenaire_id):
-        self.connection.change(f'UPDATE {PartenaireRepository.TABLE} SET name = "{partenaire.name}", description = "{partenaire.description}", type = "{partenaire.type}", image = "{partenaire.image}" WHERE id = {partenaire_id}')
+    def update(self, partenaire, name):
+        self.connection.change(f'UPDATE {PartenaireRepository.TABLE} SET name = "{partenaire.name}", description = "{partenaire.description}", type = "{partenaire.type}", image = "{partenaire.image}" WHERE name = "{name}"')
 
-    def delete(self, id):
-        self.connection.change(f'DELETE FROM {PartenaireRepository.TABLE} WHERE id = {id}')
+    def delete(self, name):
+        self.connection.change(f'DELETE FROM {PartenaireRepository.TABLE} WHERE name = "{name}"')
 
-    def activate(self, id):
-        self.connection.change(f'UPDATE {PartenaireRepository.TABLE} SET active = 1 WHERE id = {id}')
+    def activate(self, name):
+        self.connection.change(f'UPDATE {PartenaireRepository.TABLE} SET active = 1 WHERE name = "{name}"')
 
-    def deactivate(self, id):
-        self.connection.change(f'UPDATE {PartenaireRepository.TABLE} SET active = 0 WHERE id = {id}')
+    def deactivate(self, name):
+        self.connection.change(f'UPDATE {PartenaireRepository.TABLE} SET active = 0 WHERE name = "{name}"')
 
     def get_all_active(self) -> PartenaireResponseList:
         return PartenaireResponseList(self.connection.get(f'SELECT * FROM {PartenaireRepository.TABLE} WHERE active = 1'))
