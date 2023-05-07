97b090242155d066a4176db43e1f15b469586a46
Marc-Anthony Girard <marcanthony.girard.pro@gamil.com>
2023-05-07 19:22:58 -0400

    new url

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 
 export const api = (): AxiosInstance => {
   return axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'canoe-staging.fly.dev',
     headers: {
       'Access-Control-Allow-Origin': '*',
       token: document.cookie
