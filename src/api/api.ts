9489467ece5e10571ac16403c7edf197b49595f5
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-05-13 20:25:46 -0400

    resolve call to wrong url

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 
 export const api = (): AxiosInstance => {
   return axios.create({
-    baseURL: 'canoe-staging.fly.dev',
+    baseURL: 'https://canoe-staging.fly.dev/',
     headers: {
       'Access-Control-Allow-Origin': '*',
       token: document.cookie
