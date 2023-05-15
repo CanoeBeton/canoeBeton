d7146018794d98010ab89420ff65817f59b453f4
Marc-Anthony Girard <marcanthony.girard.pro@gamil.com>
2023-05-14 22:04:00 -0400

    text center

--- src/ui/bigComponents/Accueil/LastYear/LastYear.tsx
+++ src/ui/bigComponents/Accueil/LastYear/LastYear.tsx
@@ -17,7 +17,7 @@ const LastYear: FunctionComponent<PropsWithChildren<HeaderProps>> = ({ }) => {
         <h1 className={'font-bold text-5xl pb-8'}> Ce qui nous attend pour l&apos;édition 2022-2023 </h1>
         <div className={'flex gap-8 flex-col lg:flex-row'}>
           <img alt="Notre équipe" src="/Accueil/nextyear.webp" className={'lg:w-[40vw] object-contain object-center'} />
-          <text>La compétition régionale américaine à laquelle l&apos;équipe participe, l&apos;ASCE Student Concrete Canoe Competition,
+          <text className={'text-center'}>La compétition régionale américaine à laquelle l&apos;équipe participe, l&apos;ASCE Student Concrete Canoe Competition,
             est tenue habituellement en avril 2023. Une première place à cette compétition permet à l&apos;équipe de participer à la
             compétition nationale plus tard en juin 2023. La compétition nationale canadienne tenue par la SCGC a lieu en mai 2023.
             Les villes canadiennes et américaines accueillant les compétitions cette année sont encore inconnues.</text>
